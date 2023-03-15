af9f6cda883cdb211f28be30b69e6ef80ef729e4
Лилиана <lilya_gazina@mail.ru>
2023-03-15 14:40:58 +0300

    1

---
+++ src/main/java/com/calculator/calculatorApp/CalculatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.calculatorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorAppApplication.class, args);
+	}
+
+}
